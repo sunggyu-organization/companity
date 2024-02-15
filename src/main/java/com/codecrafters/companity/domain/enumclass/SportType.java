8538de468492aee9510b589431c83982f4755eba
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-15 14:56:54 +0900

    feat: Define both City and Sport enum type

--- src/main/java/com/codecrafters/companity/domain/enumclass/SportType.java
+++ src/main/java/com/codecrafters/companity/domain/enumclass/SportType.java
@@ -1,5 +1,19 @@
 package com.codecrafters.companity.domain.enumclass;
 
 public enum SportType {
-    Soccer
+    Badminton(0 ,"배드민턴"),
+    Baseball(1 ,"야구"),
+    Basketball(2 ,"농구"),
+    Billiards(3 ,"당구"),
+    Bowling(4 ,"볼링"),
+    Soccer(5 ,"축구"),
+    Running(6 ,"런닝");
+
+    int no;
+    String value;
+
+    SportType(int no, String value) {
+        this.no = no;
+        this.value = value;
+    }
 }
