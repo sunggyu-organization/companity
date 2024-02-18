a12574a260497580132342cf37aeffa8323d04aa
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-18 21:23:25 +0900

    feat: Add feature of finding all posts to PostController SCRUM-11

--- src/main/java/com/codecrafters/companity/adapter/post/controller/PostController.java
+++ src/main/java/com/codecrafters/companity/adapter/post/controller/PostController.java
@@ -1,14 +1,14 @@
 package com.codecrafters.companity.adapter.post.controller;
 
 import com.codecrafters.companity.application.in.usecase.PostUseCase;
+import com.codecrafters.companity.domain.post.OrderType;
 import com.codecrafters.companity.domain.post.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -19,5 +19,12 @@ public class PostController {
     public ResponseEntity<ResponsePost> add(@RequestBody RequestPost requestPost){
         Post result = postUseCase.add(requestPost.getPost(), requestPost.getUserId());
         return new ResponseEntity<>(new ResponsePost(result), HttpStatus.OK);
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<ResponsePost>> getAll(){
+        List<Post> all = postUseCase.findByCriteria(new Post(), OrderType.RecentDate);
+        List<ResponsePost> result = all.stream().map(ResponsePost::new).toList();
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
