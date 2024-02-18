51067d62d290aff1c81c27a548a90a9365819338
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-18 21:30:33 +0900

    feat: Define ResponsePost SCRUM-11

--- src/main/java/com/codecrafters/companity/adapter/post/controller/ResponsePost.java
+++ src/main/java/com/codecrafters/companity/adapter/post/controller/ResponsePost.java
@@ -1,9 +1,23 @@
 package com.codecrafters.companity.adapter.post.controller;
 
+import com.codecrafters.companity.adapter.user.controller.ResponseUser;
+import com.codecrafters.companity.domain.post.Comment;
 import com.codecrafters.companity.domain.post.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ResponsePost {
+    private Long id;
+    private String title;
+    private int city;
+    private int sportsTypes;
+    private String content;
+    private LocalDateTime date;
+    private boolean recruit;
+    private int likeCount;
+    private ResponseUser owner;
+    private List<Comment> comments;
     public ResponsePost(Post post){
-        //TODO need to describe
     }
 }
