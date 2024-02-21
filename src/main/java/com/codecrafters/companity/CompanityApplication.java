591257c29c129f7d490d5224044306b2e9a3b225
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-21 13:32:35 +0900

    feat: apply flywaydb and disable spring security

--- src/main/java/com/codecrafters/companity/CompanityApplication.java
+++ src/main/java/com/codecrafters/companity/CompanityApplication.java
@@ -2,8 +2,10 @@ package com.codecrafters.companity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 public class CompanityApplication {
 
 	public static void main(String[] args) {
