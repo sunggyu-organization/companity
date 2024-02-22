d24bcb1aa4528c78eebf7e5c00dd05aff0ca9843
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-22 13:12:10 +0900

    feat: add dynamic condition in PostRepositoryImpl

--- src/main/java/com/codecrafters/companity/adapter/infrastructure/jpa/post/PostRepositoryImpl.java
+++ src/main/java/com/codecrafters/companity/adapter/infrastructure/jpa/post/PostRepositoryImpl.java
@@ -5,24 +5,22 @@ import com.codecrafters.companity.domain.enums.City;
 import com.codecrafters.companity.domain.enums.Sport;
 import com.codecrafters.companity.domain.post.OrderType;
 import com.codecrafters.companity.domain.post.Post;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.codecrafters.companity.adapter.infrastructure.jpa.post.QPostEntity.postEntity;
+
 
 @Repository
+@RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepository {
     private final PostJPARepository postJPARepository;
     private final JPAQueryFactory jpaQueryFactory;
-
-    @Autowired
-    public PostRepositoryImpl(PostJPARepository postJPARepository, EntityManager entityManager){
-        jpaQueryFactory = new JPAQueryFactory(entityManager);
-        this.postJPARepository = postJPARepository;
-    }
 
     @Override
     public Post add(Post post) {
@@ -42,6 +40,35 @@ public class PostRepositoryImpl implements PostRepository {
 
     @Override
     public List<Post> findBySportAndCityAndRecruitOrderByRecentDateOrFavorite(Sport sport, City city, boolean recruit, OrderType orderType) {
+        //TODO
+//        List<PostEntity> postEntities = jpaQueryFactory.selectFrom(postEntity)
+//                .where(eqSport(sport),
+//                        eqCity(city),
+//                        postEntity.recruit.eq(recruit))
+//                .orderBy(getOrderBy(orderType))
+//                .fetch();
         return null;
     }
+
+    private BooleanExpression eqSport(Sport sport){
+        if(sport == null) return null;
+        return postEntity.sport.eq(sport);
+    }
+
+    private BooleanExpression eqCity(City city){
+        if(city == null) return null;
+        return postEntity.city.eq(city);
+    }
+
+    private OrderSpecifier getOrderBy(OrderType orderType){
+        if(orderType == null) return postEntity.createAt.desc();
+        switch (orderType){
+            case Favorite:
+                return postEntity.likeCount.desc();
+            case RecentDate:
+                return postEntity.createAt.desc();
+        }
+        return postEntity.createAt.desc();
+    }
+
 }
