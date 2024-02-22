4f6630712d84ad2b6c79bd38ba9bd42660196bb5
NohSungGyu <gkdlshtjdrb@naver.com>
2024-02-22 13:09:28 +0900

    refactor: set query dsl config

---
+++ src/main/java/com/codecrafters/companity/config/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package com.codecrafters.companity.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(em);
+    }
+}
