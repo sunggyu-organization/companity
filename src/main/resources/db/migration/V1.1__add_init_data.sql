c28b02d6b6d86ad5c4bf4c3f2ac706c4c53844fe
NohSungGyu <gkdlshtjdrb@naver.com>
2024-05-27 16:19:33 +0900

    add: Add db migration file for creating init data

---
+++ src/main/resources/db/migration/V1.1__add_init_data.sql
@@ -0,0 +1,5 @@
+--insert user data
+INSERT INTO users (user_id, username) values(1, 'shtjdrb');
+
+--insert post data
+INSERT INTO post (post_id, user_id, city, title, content, sport, like_count, recruit, create_at) VALUES(1, 1, 'Seoul', 'test', '테스트입니다', 'Soccer', 20, true, '2024-05-27T11:06:16.5905254');
